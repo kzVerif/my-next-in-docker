e25ae93f5330b84f2b081a95203a92d260117377
KzVerif <kanghunproject@gmail.com>
2026-02-11 23:53:29 +0700

    สวัสดี

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
 
-interface dataTypes {
-  id: number;
-  name: string; // Corrected type to 'string'
-  email: string;
-}
 
 export default async function Page() { // Capitalized 'Page' for convention
-  const data = await (await fetch('http://localhost:5000/users')).json();
-  console.log(data);
 
   // Return an array of JSX elements from the map function
   return (
     <>
       <p>Hello world</p>
       <br />
-      {data.map((user: dataTypes) => ( // Added parentheses for clarity
-        <ul key={user.id}>
-          <p>{user.name}</p>
-          <p>{user.email}</p>
-          <br />
-        </ul>
-      ))}
     </>
   );
 }
